d45dd3e71441345fb27396b22fbecced44d98bab
FrancescoScanni <frascanni07@gmail.com>
2025-01-22 23:41:55 +0100

    Quiz Object (v1.0 - Setup file e prima stesura)

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+
+let questions = [  //array di oggetti (dizionari - coppie chiave/valore)
+    {
+        "n_domanda": 1,
+        "testo_domanda": "..............",
+        "risposta_utente": "",
+        "risposta_corretta": "...........",
+        "punteggio": 1
+    },
+    {
+        "n_domanda": 2,
+        "testo_domanda": "..............",
+        "risposta_utente": "",
+        "risposta_corretta": "...........",
+        "punteggio": 2
+    },
+
+    //........
+
+]
